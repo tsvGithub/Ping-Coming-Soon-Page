f4bec2094ce0f8ae22f84b43c281dc631f7a1a72
tsvGithub <43065594+tsvGithub@users.noreply.github.com>
2019-07-26 16:23:56 +0200

    email validation added
    email validation added with styles change

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var regex = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+// var regex = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+var regex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
 
 function emailValid(){
       var email = document.getElementById("email");
@@ -8,16 +9,17 @@ function emailValid(){
       changeStyle(emailWarning, "Whoops! It looks like you forgot to add your email");
   }else if (!regex.test(email.value)){
       changeStyle(emailWarning, 'Please provide a valid email address');
-      email.value="";
+    //   email.value="";
   }else{
     changeStyle(emailWarning, "Thank You For Subscribing!");
-    emailWarning.style.color = "hsl(0, 0%, 59%)";
-    email.style.borderColor = "hsl(0, 0%, 59%)";
+    emailWarning.style.color = "hsl(223, 87%, 63%)";
+    email.style.borderColor = "hsl(223, 87%, 63%)";
   }
   }
 function changeStyle(id,info) {
   id.innerHTML = info;
   email.style.borderColor = "hsl(354, 100%, 66%)";
-  email.setAttribute("placeholder", "address@email.com");
+  email.setAttribute("placeholder", "example@email.com");
+  email.style.color = "hsl(209, 33%, 12%)";
 }
 
